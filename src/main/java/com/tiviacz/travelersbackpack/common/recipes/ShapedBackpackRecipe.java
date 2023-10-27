61bc9833a4269f9c56077fbf4b13a17782be34c9
HikikoBunny <149204266+HikikoBunny@users.noreply.github.com>
2023-10-27 18:30:30 -0300

    Update ShapedBackpackRecipe.java

--- src/main/java/com/tiviacz/travelersbackpack/common/recipes/ShapedBackpackRecipe.java
+++ src/main/java/com/tiviacz/travelersbackpack/common/recipes/ShapedBackpackRecipe.java
@@ -3,7 +3,6 @@ package com.tiviacz.travelersbackpack.common.recipes;
 import com.google.gson.JsonObject;
 import com.tiviacz.travelersbackpack.blocks.SleepingBagBlock;
 import com.tiviacz.travelersbackpack.items.SleepingBagItem;
-import com.tiviacz.travelersbackpack.items.TravelersBackpackItem;
 import com.tiviacz.travelersbackpack.util.RecipeUtils;
 import net.minecraft.core.NonNullList;
 import net.minecraft.core.RegistryAccess;
@@ -19,30 +18,7 @@ import net.minecraftforge.common.crafting.CraftingHelper;
 
 import javax.annotation.Nullable;
 
-public class ShapedBackpackRecipe extends ShapedRecipe
-{
-    public ShapedBackpackRecipe(ResourceLocation idIn, String groupIn, CraftingBookCategory category, int recipeWidthIn, int recipeHeightIn, NonNullList<Ingredient> recipeItemsIn, ItemStack recipeOutputIn)
-    {
-        super(idIn, groupIn, category, recipeWidthIn, recipeHeightIn, recipeItemsIn, recipeOutputIn);
-    }
 
-    @Override
-    public ItemStack assemble(CraftingContainer inv, RegistryAccess access)
-    {
-        final ItemStack output = super.assemble(inv, access);
-
-        if(!output.isEmpty())
-        {
-            for(int i = 0; i < inv.getContainerSize(); i++)
-            {
-                final ItemStack ingredient = inv.getItem(i);
-
-                if(!ingredient.isEmpty() && ingredient.getItem() instanceof TravelersBackpackItem)
-                {
-                    final CompoundTag compound = ingredient.getTag();
-                    output.setTag(compound);
-                    break;
-                }
 
                 if(!ingredient.isEmpty() && ingredient.getItem() instanceof SleepingBagItem)
                 {
@@ -62,64 +38,6 @@ public class ShapedBackpackRecipe extends ShapedRecipe
         return DyeColor.RED.getId();
     }
 
-    @Override
-    public RecipeSerializer<?> getSerializer()
-    {
-        return Serializer.INSTANCE;
-    }
-
-    @Override
-    public RecipeType<?> getType() {
-        return RecipeType.CRAFTING;
-    }
-
-    public static class Serializer implements RecipeSerializer<ShapedBackpackRecipe>
-    {
-        public static final Serializer INSTANCE = new Serializer();
-
-        @Override
-        public ShapedBackpackRecipe fromJson(ResourceLocation recipeID, JsonObject json)
-        {
-            final String group = GsonHelper.getAsString(json, "group", "");
-            CraftingBookCategory craftingbookcategory = CraftingBookCategory.CODEC.byName(GsonHelper.getAsString(json, "category", (String)null), CraftingBookCategory.MISC);
-            final RecipeUtils.ShapedPrimer primer = RecipeUtils.parseShaped(json);
-            final ItemStack result = CraftingHelper.getItemStack(GsonHelper.getAsJsonObject(json, "result"), true);
-
-            return new ShapedBackpackRecipe(recipeID, group, craftingbookcategory, primer.getRecipeWidth(), primer.getRecipeHeight(), primer.getIngredients(), result);
-        }
-
-        @Nullable
-        @Override
-        public ShapedBackpackRecipe fromNetwork(ResourceLocation recipeID, FriendlyByteBuf buffer)
-        {
-            final int width = buffer.readVarInt();
-            final int height = buffer.readVarInt();
-            final String group = buffer.readUtf(Short.MAX_VALUE);
-            CraftingBookCategory craftingbookcategory = buffer.readEnum(CraftingBookCategory.class);
-            final NonNullList<Ingredient> ingredients = NonNullList.withSize(width * height, Ingredient.EMPTY);
-
-            for (int i = 0; i < ingredients.size(); ++i) {
-                ingredients.set(i, Ingredient.fromNetwork(buffer));
-            }
-
-            final ItemStack result = buffer.readItem();
-
-            return new ShapedBackpackRecipe(recipeID, group, craftingbookcategory, width, height, ingredients, result);
-        }
-
-        @Override
-        public void toNetwork(FriendlyByteBuf buffer, ShapedBackpackRecipe recipe)
-        {
-            buffer.writeVarInt(recipe.getRecipeWidth());
-            buffer.writeVarInt(recipe.getRecipeHeight());
-            buffer.writeUtf(recipe.getGroup());
-            buffer.writeEnum(recipe.category());
-
-            for (final Ingredient ingredient : recipe.getIngredients()) {
-                ingredient.toNetwork(buffer);
-            }
-
-            buffer.writeItemStack(recipe.result, false);
-        }
+       }
     }
 }
