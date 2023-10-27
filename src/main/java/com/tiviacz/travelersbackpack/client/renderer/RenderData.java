3f93c4b9e68b28a843c9ec8e4fe73292ae22d408
HikikoBunny <149204266+HikikoBunny@users.noreply.github.com>
2023-10-27 18:07:36 -0300

    Update RenderData.java

--- src/main/java/com/tiviacz/travelersbackpack/client/renderer/RenderData.java
+++ src/main/java/com/tiviacz/travelersbackpack/client/renderer/RenderData.java
@@ -1,22 +1,15 @@
 package com.tiviacz.travelersbackpack.client.renderer;
 
-import com.tiviacz.travelersbackpack.inventory.Tiers;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.fluids.FluidStack;
-import net.minecraftforge.fluids.capability.templates.FluidTank;
 
 public class RenderData
 {
     private final ItemStack stack;
     private final Player player;
-    private final FluidTank leftTank = createFluidHandler();
-    private final FluidTank rightTank = createFluidHandler();
 
-    private final String LEFT_TANK = "LeftTank";
-    private final String RIGHT_TANK = "RightTank";
     private final String COLOR = "Color";
     private final String SLEEPING_BAG_COLOR = "SleepingBagColor";
 
@@ -31,20 +24,6 @@ public class RenderData
         }
     }
 
-    public FluidTank getLeftTank()
-    {
-        return this.leftTank;
-    }
-
-    public FluidTank getRightTank()
-    {
-        return this.rightTank;
-    }
-
-    public ItemStack getItemStack()
-    {
-        return this.stack;
-    }
 
     public int getSleepingBagColor()
     {
@@ -53,34 +32,5 @@ public class RenderData
             return this.stack.getOrCreateTag().getInt(SLEEPING_BAG_COLOR);
         }
         return DyeColor.RED.getId();
-    }
-
-    public void loadDataFromStack(ItemStack stack)
-    {
-        if(!stack.isEmpty() && stack.hasTag())
-        {
-            loadTanks(stack.getOrCreateTag());
-        }
-    }
-
-    public void loadTanks(CompoundTag compound)
-    {
-        this.leftTank.readFromNBT(compound.getCompound(LEFT_TANK));
-        this.rightTank.readFromNBT(compound.getCompound(RIGHT_TANK));
-    }
-
-    private FluidTank createFluidHandler()
-    {
-        return new FluidTank(Tiers.LEATHER.getTankCapacity())
-        {
-            @Override
-            public FluidTank readFromNBT(CompoundTag nbt)
-            {
-                FluidStack fluid = FluidStack.loadFluidStackFromNBT(nbt);
-                setCapacity(Tiers.of(RenderData.this.stack.getOrCreateTag().getInt(Tiers.TIER)).getTankCapacity());
-                setFluid(fluid);
-                return this;
-            }
-        };
     }
 }
